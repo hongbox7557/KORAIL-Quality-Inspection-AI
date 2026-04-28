09c218bf7160ae8474574ba35c7ceb27f7c33116
hongbox7557 <hongbox7557@gmail.com>
2026-04-28 18:58:05 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,11 @@
+import google.generativeai as genai
+
+genai.configure(api_key=st.secrets["GEMINI_API_KEY"])
+
+# 사용 가능한 모델 출력
+for m in genai.list_models():
+    if 'generateContent' in m.supported_generation_methods:
+        print(m.name)
 import streamlit as st
 import google.generativeai as genai
 
